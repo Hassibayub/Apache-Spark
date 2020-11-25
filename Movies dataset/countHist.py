3d492adf9cbba7f88da0e7b57525eaed01bed91c
hassibayub <hassibayub@gmail.com>
2020-11-25 19:43:12 +0500

    count histogram movie rating

---
+++ Movies dataset/countHist.py
@@ -0,0 +1,26 @@
+from pyspark import SparkConf, SparkContext 
+import collections
+import time
+
+start = time.perf_counter()
+
+conf = SparkConf().setMaster("local").setAppName("RatingCounter")
+sc = SparkContext(conf = conf)
+
+try:
+    lines = sc.textFile("./ml-100k/u.data")
+    rating = lines.map(lambda x: x.split()[2])
+    result = rating.countByValue()
+except:
+    lines = sc.textFile("./Movies dataset/ml-100k/u.data")
+    rating = lines.map(lambda x: x.split()[2])
+    result = rating.countByValue()
+    
+
+
+sortedResults = collections.OrderedDict(sorted( result.items() )) 
+
+for key, value in sortedResults.items():
+    print("{}, {}".format(key, value))
+    
+print("Total time took: ", time.perf_counter() - start)
