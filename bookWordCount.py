0fcd0bc3d24ca9dca4894aeddd40c950e0fa2e3b
hassibayub <hassibayub@gmail.com>
2020-11-26 15:24:41 +0500

    regex added | fine results

--- bookWordCount.py
+++ bookWordCount.py
@@ -1,11 +1,12 @@
 from pyspark import SparkConf, SparkContext 
 import collections
+import re 
 
 conf = SparkConf().setMaster("local").setAppName("bookWordCount")
 sc = SparkContext(conf= conf)
 
 raw = sc.textFile('./Book.txt')
-words = raw.flatMap(lambda x: x.split())
+words = raw.flatMap(lambda x: re.compile(r'\W+', re.UNICODE).split(x.lower()))
 counter = words.countByValue()
 counter = dict(sorted(counter.items()))
 
