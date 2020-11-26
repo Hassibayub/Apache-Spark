0c8b71335e14a818e047dffe6d42d0e0fb624687
hassibayub <hassibayub@gmail.com>
2020-11-26 13:00:54 +0500

    "Avg Friends by Age"

--- fakeFrindsSpark.py
+++ fakeFrindsSpark.py
@@ -1,14 +1,10 @@
 from pyspark import SparkConf, SparkContext
+from time import perf_counter
+
+start = perf_counter()
 
 conf = SparkConf().setMaster("local").setAppName("FakeFriends")
 sc = SparkContext(conf= conf)
-
-def parseData(val):
-    data = val.split()
-    age = int(data[2])
-    friends = int(data[3])
-    return (age,friends)
-
 
 raw = sc.textFile(r"G:\Shared drives\Unlimited\Python Scripts\Apache Spark\fakefriends.csv")
 
@@ -16,12 +12,14 @@ datapair = raw.map(lambda x: ( int(x.split(",")[2]), int(x.split(",")[3]) ))
 # datapaint (age, friends)
 
 aggData  = datapair.mapValues(lambda x: (x,1)).reduceByKey(lambda x,y : (x[0] + y[0], x[1]+y[1] ))
-# # aggData (eachAge , (totalfriends, counter))
+# aggData (eachAge , (totalfriends, counter))
 
 avgData = aggData.mapValues(lambda x: x[0]/x[1])
-# # # avgData (age, avgFriends)
+# avgData (age, avgFriends)
 
 dataCollected = avgData.collect()
 
 for age, avgFrinds in dataCollected:
     print("At Age {}, Avg Friends {}".format(age, int(avgFrinds)))
+    
+print("Time elapsed: ", perf_counter() - start, " secs")
